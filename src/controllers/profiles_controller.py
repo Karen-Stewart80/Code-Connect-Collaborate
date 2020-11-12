2b5a83c6d2d8bcc0a97d712e28833896d4cfb7b8
Karen-Stewart <kazstewart911@hotmail.com>
2020-11-12 17:07:44 +1000

    post request not working

--- src/controllers/profiles_controller.py
+++ src/controllers/profiles_controller.py
@@ -9,3 +9,23 @@ profiles = Blueprint("profiles", __name__, url_prefix="/profiles")
 def profiles_index():
     profiles = Profiles.query.all()
     return jsonify(profiles_schema.dump(profiles))
+
+
+@profiles.route("/", methods=["POST"])
+def profiles_create():
+    users_fields = profile_schema.load(request.json)
+
+    new_user = Profiles()
+    new_user.username = users_fields["username"]
+    new_user.fname = users_fields["fname"]
+    new_user.lname = users_fields["lname"]
+    new_user.userpass = users_fields["userpass"]
+    new_user.profile_pic = users_fields["profile_pic"]
+    new_user.account_active = users_fields["account_active"]
+    new_user.email = users_fields["email"]
+    new_user.github = users_fields["github"]
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify(user_schema.dump(new_user))
